82560b3c01261bdc37190c3f48674205a2dd2303
florianleriche <florian.lrch@gmail.com>
2025-06-26 15:17:17 +0200

    fix privateKey

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import TelegramBot from 'node-telegram-bot-api';
 import { exec } from 'child_process';
 import path from 'path';
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const APP_ADDRESS = '0xDe32125D50735A111A535deb12ac664F90466e9C';
+export const APP_ADDRESS = '0x0380F06Ba0208b8f50F04DB7236cAc8f20bB65cC';
 export const WORKERPOOL_ADDRESS = 'prod-v8-learn.main.pools.iexec.eth';
